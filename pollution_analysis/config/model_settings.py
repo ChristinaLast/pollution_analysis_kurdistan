bcac1e17c9bc34edd9ebd99662d78b29e181c699
ChristinaLast <christina.last@outlook.com>
2022-03-12 23:35:35 +0000

    :wrench: adding module configurations

--- pollution_analysis/config/model_settings.py
+++ pollution_analysis/config/model_settings.py
@@ -1,5 +1,8 @@
 import os
 from pydantic.dataclasses import dataclass
+from dataclasses import field
+from typing import Dict, Tuple, Sequence
+from pydantic import StrictStr
 
 
 @dataclass
@@ -23,3 +26,38 @@ class FlaringLoaderConfig:
 @dataclass
 class FlaringDescriptorConfig:
     PROCESSED_TARGET_DIR = "iraq_processed_data/local_data"
+
+
+@dataclass
+class FlaringGrouperConfig:
+    PROCESSED_TARGET_DIR: str = "iraq_processed_data/local_data"
+    FLARING_COLUMNS_TO_KEEP: Sequence[str] = field(
+        default_factory=lambda: [
+            "id",
+            "id_Key",
+            "Date_Proc",
+            "Lat_GMTCO",
+            "Lon_GMTCO",
+            "Date_LTZ",
+            "Cloud_Mask",
+        ]
+    )
+
+
+@dataclass
+class MethaneLoaderConfig:
+    COUNTRY_CODES = ["IQ"]
+    CRS: str = "epsg:4326"
+    YEAR: int = 2019
+    MON_START: int = 7
+    DATE_START: int = 13
+    YEAR_END: int = 2022
+    MON_END: int = 3
+    DATE_END: int = 11
+    PLACE = "Iraqi Kurdistan, Iraq"
+    BASE_FOLDER = "/ee_data"
+    METHANE_IMAGE_COLLECTION = "COPERNICUS/S5P/OFFL/L3_CH4"
+    METHANE_IMAGE_BAND = [
+        "CH4_column_volume_mixing_ratio_dry_air",
+    ]
+    MODEL_NAME = "CH4"
