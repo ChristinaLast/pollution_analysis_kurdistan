a8da487e7513a0bb9489d828bee15a92f32b0d14
ChristinaLast <christina.last@outlook.com>
2022-03-15 20:00:12 +0000

    :bug: fixing script

--- scripts/aggregate_methane_locations.py
+++ scripts/aggregate_methane_locations.py
@@ -6,8 +6,27 @@ import pandas as pd
 from open_geo_engine.utils.utils import read_csv, write_csv
 
 
-def concatenate_methane_data(filepath, methane_df_list):
-    return methane_df_list.append(read_csv(filepath))
+def concatenate_methane_data(filepath):
+    names = [
+        "longitude",
+        "latitude",
+        "time",
+        "datetime",
+        "CH4_column_volume_mixing_ratio_dry_air",
+    ]
+    methane_df = read_csv(f"ee_data/{filepath}")
+    if list(methane_df.columns) != names:
+        return read_csv(f"ee_data/{filepath}", header=names)
+    else:
+        return methane_df
+    # print(methane_df)
+    # return methane_df_list.append(methane_df)
+
+
+def has_header(file, nrows=20):
+    df = pd.read_csv(file, header=None, nrows=nrows)
+    df_header = pd.read_csv(file, nrows=nrows)
+    return tuple(df.dtypes) != tuple(df_header.dtypes)
 
 
 if __name__ == "__main__":
@@ -20,9 +39,15 @@ if __name__ == "__main__":
         if isfile(join("ee_data", filename))
         and not any(filetype in filename for filetype in non_flaring_filetypes)
     ]
-    methane_df_list = []
-    methane_by_date_list = Parallel(n_jobs=-1, backend="multiprocessing", verbose=5)(
-        delayed(concatenate_methane_data)(filepath, methane_df_list) for filepath in methane_filepaths
+    methane_by_date_df = pd.concat(
+        Parallel(n_jobs=-1, backend="multiprocessing", verbose=5)(
+            delayed(concatenate_methane_data)(filepath)
+            for filepath in methane_filepaths
+        )
     )
-    methane_by_date_df = pd.concat(methane_by_date_list)
-    write_csv(methane_by_date_df.drop_duplicates(), "summarised_data/kurdistan_processed_data/methane_concentrations.csv")
+    print(methane_by_date_df)
+    # methane_by_date_df = pd.concat(methane_by_date_list)
+    write_csv(
+        methane_by_date_df.drop_duplicates(),
+        "summarised_data/kurdistan_processed_data/methane_concentrations.csv",
+    )
