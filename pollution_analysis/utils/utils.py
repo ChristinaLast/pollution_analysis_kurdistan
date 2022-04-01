4476e0b1ffb4bf308c82d03af10cae6bd2d624a9
ChristinaLast <christina.last@outlook.com>
2022-04-01 04:27:26 +0100

    :wrench: adding utility file to store reuseable functions

--- pollution_analysis/utils/utils.py
+++ pollution_analysis/utils/utils.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 from typing import Any
 
 import pandas as pd
+import geopandas as gpd
 
 
 def date_range(start, end):
@@ -18,6 +19,14 @@ def read_csv(path: str, **kwargs: Any) -> pd.DataFrame:
     return pd.read_csv(
         path, sep=",", low_memory=False, encoding="utf-8", na_filter=False, **kwargs
     )
+
+
+def read_csv_to_gdf(path: str, df_crs, geom_col, **kwargs: Any) -> gpd.GeoDataFrame:
+    """
+    Read csv to a gpd.GeoDataFrame ensuring that nan's are not parsed
+    """
+    df = read_csv(path)
+    return gpd.GeoDataFrame(df, crs=df_crs, geometry=geom_col, **kwargs)
 
 
 def write_csv(df: pd.DataFrame, path: str, **kwargs: Any) -> None:
