8e90ac2e22c16ad273713dd97b92bd5c9f9acf1b
ChristinaLast <christina.last@outlook.com>
2022-11-27 00:02:52 -0500

    adding script updates

--- pollution_analysis/src/flaring_loader.py
+++ pollution_analysis/src/flaring_loader.py
@@ -54,10 +54,14 @@ class FlaringLoader:
             delayed(self.execute_for_year)(
                 country_gdf, country_name, dirname, containing_folder, fileList
             )
-            for dirname, containing_folder, fileList in os.walk(self.raw_data_dir)
+            for dirname, containing_folder, fileList in os.walk(
+                self.raw_data_dir
+            )
         )
 
-    def execute_for_year(self, gdf, country_name, dirname, containing_folder, fileList):
+    def execute_for_year(
+        self, gdf, country_name, dirname, containing_folder, fileList
+    ):
         """
         This executes the `FlaringLoader` pipeline for each year folder
         in the specified directory.
@@ -91,9 +95,13 @@ class FlaringLoader:
         flaring_filepaths = [
             os.path.join(dirname, filename)
             for filename in fileList
-            if not any(filetype in filename for filetype in non_flaring_filetypes)
+            if not any(
+                filetype in filename for filetype in non_flaring_filetypes
+            )
         ]
-        for date in pd.date_range(self.start_date, self.end_date).strftime("%Y%m%d"):
+        for date in pd.date_range(self.start_date, self.end_date).strftime(
+            "%Y%m%d"
+        ):
             filepaths_within_daterange = [
                 filepath for filepath in flaring_filepaths if date in filepath
             ]
@@ -110,15 +118,16 @@ class FlaringLoader:
                     parents=True, exist_ok=True
                 )
                 country_flaring_gdf.to_csv(
-                    f"processed_data/{country_name}_data/{self._get_dates_from_files(filepath)}.csv"
+                    f"processed_data/{country_name}/{self._get_dates_from_files(filepath)}.csv"
                 )
 
     def _unzip_to_df(self, filepath):
         try:
             csv = read_csv(filepath, error_bad_lines=False)
+            return csv
         except UnicodeError:
             print(filepath)
-        return csv
+            pass
 
     def _read_gdf(self):
         gdf = gpd.read_file(self.country_shp)
@@ -129,7 +138,9 @@ class FlaringLoader:
         return gpd.GeoDataFrame(
             flaring_df,
             crs=4326,
-            geometry=gpd.points_from_xy(flaring_df.Lon_GMTCO, flaring_df.Lat_GMTCO),
+            geometry=gpd.points_from_xy(
+                flaring_df.Lon_GMTCO, flaring_df.Lat_GMTCO
+            ),
         )
 
     def _get_xy(self, flaring_df):
